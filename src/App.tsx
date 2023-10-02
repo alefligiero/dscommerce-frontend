53a89f9f6c5e998962d3801036097a5f525a3801
Alef Ligiero <alefligieroagb@gmail.com>
2023-10-02 20:35:42 -0300

    cart localstorage save/get

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ProductCatalog from "./routes/ClientHome/ProductCatalog";
 import ClientHome from "./routes/ClientHome";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
+import Cart from "./routes/ClientHome/Cart";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
           <Route index element={<ProductCatalog />} />
           <Route path="product-catalog" element={<ProductCatalog />} />
           <Route path="product-details/:productId" element={<ProductDetails />} />
+          <Route path="cart" element={<Cart />}/>
         </Route>
         <Route path="*" element={<Navigate to={"/"}/>}></Route>
       </Routes>
