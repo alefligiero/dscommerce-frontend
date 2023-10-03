616da0a6435339c358a3ad6f7453576f899a7df3
Alef Ligiero <alefligieroagb@gmail.com>
2023-10-02 21:07:32 -0300

    add product on cart and clear cart

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
 import * as productService from "../../../services/product-service";
+import * as cartService from "../../../services/cart-service";
 
 export default function ProductDetails() {
   const params = useParams();
@@ -16,13 +17,22 @@ export default function ProductDetails() {
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(() => {
-    productService.findById(Number(params.productId)).then((response) => {
-      setProduct(response.data);
-    })
-    .catch(() => {
-      navigate("/");
-    });
+    productService
+      .findById(Number(params.productId))
+      .then((response) => {
+        setProduct(response.data);
+      })
+      .catch(() => {
+        navigate("/");
+      });
   }, []);
+
+  function handleBuyClick() {
+    if (product) {
+      cartService.addProduct(product);
+      navigate("/cart");
+    }
+  }
 
   return (
     <>
@@ -30,7 +40,9 @@ export default function ProductDetails() {
         <section id="product-details-section" className="dsc-container">
           {product && <ProductDetailsCard product={product} />}
           <div className="dsc-btn-page-container">
-            <ButtonPrimary text="Comprar" />
+            <div onClick={handleBuyClick}>
+              <ButtonPrimary text="Comprar" />
+            </div>
             <Link to={"/"}>
               <ButtonInverse text="Início" />
             </Link>
