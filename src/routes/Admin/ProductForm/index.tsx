466a3abfd8cbef53e79a1886bf4db7f368ac1282
Alef Ligiero <alefligieroagb@gmail.com>
2023-11-22 20:00:28 -0300

    Product form CRUD static layout

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,5 +1,61 @@
+import './styles.css';
+
 export default function ProductForm() {
-    return (
-        <h1>Opa, tudo bao?</h1>
-    );
+  return (
+    <main>
+      <section id="product-form-section" className="dsc-container">
+        <div className="dsc-product-form-container">
+          <form className="dsc-card dsc-form">
+            <h2>Dados do produto</h2>
+            <div className="dsc-form-controls-container">
+              <div>
+                <input
+                  className="dsc-form-control"
+                  type="text"
+                  placeholder="Nome"
+                />
+              </div>
+              <div>
+                <input
+                  className="dsc-form-control"
+                  type="text"
+                  placeholder="Preço"
+                />
+              </div>
+              <div>
+                <input
+                  className="dsc-form-control"
+                  type="text"
+                  placeholder="Imagem"
+                />
+              </div>
+              <div>
+                <select className="dsc-form-control dsc-select" required>
+                  <option value="" disabled selected>
+                    Categorias
+                  </option>
+                  <option value="">Valor 1</option>
+                  <option value="">Valor 2</option>
+                </select>
+              </div>
+              <div>
+                <textarea
+                  className="dsc-form-control dsc-textarea"
+                  placeholder="Descrição"
+                ></textarea>
+              </div>
+            </div>
+            <div className=" dsc-product-form-buttons">
+              <button type="submit" className="dsc-btn dsc-btn-white">
+                Cancelar
+              </button>
+              <button type="submit" className="dsc-btn dsc-btn-blue">
+                Salvar
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </main>
+  );
 }
