50cb4052c63cd8b0eac29c7c98f6fda33232932e
Alef Ligiero <alefligieroagb@gmail.com>
2023-12-19 17:51:50 -0300

    Custom form input

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -1,39 +1,60 @@
-import './styles.css';
-import { ChangeEvent, FormEvent, useContext, useState } from 'react';
-import { CredentialsDTO } from '../../../models/auth';
-import * as authService from '../../../services/auth-service';
-import { useNavigate } from 'react-router-dom';
-import { ContextToken } from '../../../utils/content-token';
+import { ChangeEvent, FormEvent, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import FormInput from "../../../components/FormInput";
+import * as authService from "../../../services/auth-service";
+import { ContextToken } from "../../../utils/content-token";
+import "./styles.css";
 
 export default function Login() {
+  const { setContextTokenPayload } = useContext(ContextToken);
 
-    const { setContextTokenPayload } = useContext(ContextToken);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [formData, setFormData] = useState<any>({
+    username: {
+      value: "",
+      id: "username",
+      name: "username",
+      type: "text",
+      placeholder: "Email",
+      validation: function (value: string) {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+          value.toLowerCase()
+        );
+      },
+      message: "Favor informar um email válido",
+    },
+    password: {
+      value: "",
+      id: "password",
+      name: "password",
+      type: "password",
+      placeholder: "Senha",
+    },
+  });
 
-    const [formData, setFormData] = useState<CredentialsDTO>({
-        username: '',
-        password: ''
-    });
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    authService
+      .loginRequest({
+        username: formData.username.value,
+        password: formData.password.value,
+      })
+      .then((response) => {
+        authService.saveAccessToken(response.data.access_token);
+        setContextTokenPayload(authService.getAccessTokenPayload());
+        navigate("/cart");
+      })
+      .catch((error) => {
+        console.log("Erro no login", error);
+      });
+  }
 
-    function handleSubmit(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        authService.loginRequest(formData)
-            .then(response => {
-                authService.saveAccessToken(response.data.access_token);
-                setContextTokenPayload(authService.getAccessTokenPayload());
-                navigate("/cart");
-            })
-            .catch(error => {
-                console.log("Erro no login", error);
-            });
-    }
-
-    function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
-        const value = event.target.value;
-        const name = event.target.name;
-        setFormData({...formData, [name]: value});
-    }
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({ ...formData, [name]: { ...formData[name], value: value } });
+  }
 
   return (
     <main>
@@ -43,23 +64,17 @@ export default function Login() {
             <h2>Login</h2>
             <div className="dsc-form-controls-container">
               <div>
-                <input
-                  name="username"
-                  value={formData.username}
+                <FormInput
+                  {...formData.username}
                   className="dsc-form-control"
-                  type="text"
-                  placeholder="Email"
                   onChange={handleInputChange}
                 />
                 <div className="dsc-form-error"></div>
               </div>
               <div>
-                <input
-                  name="password"
-                  value={formData.password}
+                <FormInput
+                  {...formData.password}
                   className="dsc-form-control"
-                  type="password"
-                  placeholder="Senha"
                   onChange={handleInputChange}
                 />
               </div>
