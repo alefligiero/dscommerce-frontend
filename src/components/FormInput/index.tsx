cefc60792698f6602c55d90f5ff0cea84bcdc967
Alef Ligiero <alefligieroagb@gmail.com>
2023-12-21 20:33:38 -0300

    forms toDirty, dirty behavior

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -1,5 +1,22 @@
 export default function FormInput(props: any) {
-  const { validation, invalid, ...inputProps } = props;
+  const {
+    validation,
+    invalid = "false",
+    dirty = "false",
+    onTurnDirty,
+    ...inputProps
+  } = props;
 
-  return <input {...inputProps} data-invalid={invalid} />;
+  function handleBlur() {
+    onTurnDirty(props.name);
+  }
+
+  return (
+    <input
+      onBlur={handleBlur}
+      {...inputProps}
+      data-invalid={invalid}
+      data-dirty={dirty}
+    />
+  );
 }
